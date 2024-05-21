4ccc533a62e66c7122a888cd235c79c9c854d039
Muqadsa Riaz <imac@imacs-iMac.local>
2024-05-21 19:05:51 +0500

    vg

---
+++ components/analytic_carousel.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Link from "next/link";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+export default function Analytics_carousel({ data }) {
+  const settings = {
+    dots: true,
+    nav: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: (
+      <img
+        className="slick-arrow"
+        src="/images/arrow-left.svg"
+        alt="Previous"
+      />
+    ),
+    nextArrow: (
+      <img className="slick-arrow" src="/images/arrow-right.svg" alt="Next" />
+    ),
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <>
+      <Slider {...settings}>
+        {data.map((val) => {
+          return (
+            <div className="item" key={val.id}>
+              <div className="image">
+                <img src={val.image} />
+                <p src={val.text}></p>
+              </div>
+            </div>
+          );
+        })}
+      </Slider>
+    </>
+  );
+}
