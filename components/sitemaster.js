60b472c9a2c06faa8d952bf56ac30a714778b42b
herosolutions94 <herosolutions.tk@gmail.com>
2024-05-21 19:27:10 +0500

    homepage

--- components/sitemaster.js
+++ components/sitemaster.js
@@ -6,8 +6,8 @@ export default function SiteMaster() {
     return (
         <Head>
             <title>Basil System</title>
-            <meta name="title" content="Hire & Explore" />
-            <meta name="description" content="Hire & Explore" />
+            <meta name="title" content="Where Data Meets Discovery" />
+            <meta name="description" content="Where Data Meets Discovery" />
             <link rel="icon" href="/favicon.ico" />
         </Head>
     )
